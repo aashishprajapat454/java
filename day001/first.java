364db8befe4062a4db772b9781ae84399fddfbef
Aashish Prajapat <aashishprajapat454@gmail.com>
2025-03-11 14:17:52 +0530

    add files

---
+++ day001/first.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class first
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("inter a number");
+        int num = sc.nextInt();
+
+        System.out.println("My First Java Program."+ num);
+    }
+}
---
+++ day002/thread.java
@@ -0,0 +1,5 @@
+class thread{
+    Public static void main(String[] args){
+        
+    }
+}
