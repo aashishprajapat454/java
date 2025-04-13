0698cd70c5490fd8fac387e0862315cab7f57370
Aashish Prajapat <aashishprajapat454@gmail.com>
2025-04-13 13:14:00 +0530

    in project

---
+++ project02/largestNnaturalNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class largestNnaturalNumber {
+    public static void main(String[] args) {
+        
+        // Veriable declareation
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of Elements : ");
+        int number = sc.nextInt();
+        int[] numArray = new int[number];
+        System.out.println("Enter " + number + " Numbers");
+        int maxNum = numArray[0];
+
+        // for user input
+        for (int i = 0; i < number; i++) {
+            numArray[i] = sc.nextInt();
+            
+        }
+
+        // chack largest number and store in a veriabel
+        for(int i = 1; i< number;i++){
+            if(numArray[i]>maxNum){
+                maxNum = numArray[i];
+            }
+        }
+
+        // final output is here
+        System.out.println("The largest number is : " + maxNum);
+    }
+}
